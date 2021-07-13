d16d1b4955d10a91698d91972d42f742a9ae0802
hassibatej <hassibatej@gmail.com>
2021-07-13 11:08:27 +0200

    modified the objective and README files to match

--- objective.py
+++ objective.py
@@ -2,21 +2,31 @@ from benchopt import BaseObjective
 
 
 class Objective(BaseObjective):
-    name = "Ordinary Least Squares"
+    name = "Total Variation"
 
     parameters = {
         'fit_intercept': [False],
+        'reg': [.1, .5]
     }
 
-    def __init__(self, fit_intercept=False):
+    def __init__(self, reg, fit_intercept=False):
         self.fit_intercept = fit_intercept
+        self.reg = reg
 
     def set_data(self, X, y):
         self.X, self.y = X, y
+        self.lmbd = self.reg * self._get_lambda_max()
+
+    def _beta_diff(beta):
+        return abs([b-a for a, b in zip(beta[:-1], beta[1:])])
 
     def compute(self, beta):
         diff = self.y - self.X.dot(beta)
-        return .5 * diff.dot(diff)
+        diff_beta = [b-a for a, b in zip(beta[:-1], beta[1:])]
+        return .5 * diff.dot(diff) + self.lmbd * sum(diff_beta)
+
+    def _get_lambda_max(self):
+        return abs(self.X.T.dot(self.y)).max()
 
     def to_dict(self):
-        return dict(X=self.X, y=self.y, fit_intercept=self.fit_intercept)
+        return dict(X=self.X, y=self.y, lmbd=self.lmbd)
